d9971cf4015d73b1949cbfbce5175959f168759c
Leon Christopher Hunter <xleonhunter@gmail.com>
2019-04-13 11:28:19 -0400

    Create whileloop.js

---
+++ whileloop.js
@@ -0,0 +1,10 @@
+myArray = ["The", "Quick", "Brown", "Fox", "Jumps"]
+
+counter = 0;
+numberOfElements = myArray.length;
+while(counter < numberOfElements) {
+    currentElement = myArray[counter];
+    console.log(currentElement);
+    
+    counter++;
+}
